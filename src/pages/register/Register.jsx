aed5168fb2b8c4089152d5e0b6fcf47af1703863
thienminhkl <watermahou@gmail.com>
2023-06-13 22:04:11 +0700

    testing error 404

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -88,7 +88,7 @@ function Register() {
 
                             <div className="form-group">
                                 <label className='label_form' htmlFor="password">Password</label>
-                                <input type="password" className='form-control input_form' placeholder='Password'
+                                <input type="password" className='form-control input_form' placeholder='Passwordss'
                                        id='password' name='password'
                                        onChange={formik.handleChange}
                                        onBlur={formik.handleBlur}
